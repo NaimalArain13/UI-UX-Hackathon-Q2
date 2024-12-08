b24ab8c745d185c1eea2dec7811a7ea67de31307
NaimalArain13 <naimalarain13@gmail.com>
2024-12-09 00:38:02 +0500

    updated HomePage section

--- app/Component/homepage/sardi.tsx
+++ app/Component/homepage/sardi.tsx
@@ -10,11 +10,11 @@ function SardiSection() {
    <Image 
       src={"/images/sardi.png"} alt='green image' width={443} height={300} className='flex justify-center self-center'/>
    </div>
-    <div className='md:w-1/2 py-7 w-full flex flex-col justify-center items-center sm:items-start gap-5'>
+    <div className='md:w-1/2 py-7 w-full flex flex-col justify-center items-center text-center md:text-left sm:items-start gap-5'>
         <p className=''>SUMMER 2020</p>
         <h1 className='font-semibold text-[48px]'>Part of the Neural 
         Universe</h1>
-        <p className='text-left'>We know how large objects will act, 
+        <p className=''>We know how large objects will act, 
         but things on a small scale.</p>
         <div className='flex gap-4 flex-col md:flex-row justify-center items-center sm:items-start'>
         <Button className='text-white bg-[#2DC071] p-5 rounded-none'>BUY NOW</Button>
